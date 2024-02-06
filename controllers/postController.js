62de441d5f4b19ea2dd12fd886202ebd88aff3d8
hba <87194281+hubzhooba@users.noreply.github.com>
2024-02-06 17:24:54 +0800

    Add failing test for Module A

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,32 +1,22 @@
-const postModel = require('../models/post');
+const postModel = require("../models/post");
 // const { validationResult } = require('express-validator');
 
 exports.addPost = (req, res) => {
   // const errors = validationResult(req);
 
   // if (errors.isEmpty()) {
-    const {
-      title,
-      content
-    } = req.body;
+  const { title, content } = req.body;
 
-    const author = req.session.user;
+  const author = req.session.user;
 
-    postModel.create({ title, content, author}, (err, post) => {
-      if (err) {
-        req.flash('error_msg', 'Could not create post. Please try again.');
-        res.redirect('/posts/add');
-      } else {
-        res.redirect('/posts');
-      }
-    });
-  } else {
-    const messages = errors.array().map((item) => item.msg);
-
-    req.flash('error_msg', messages.join(' '));
-    res.redirect('/posts/add');
-  }
-
+  postModel.create({ title, content, author }, (err, post) => {
+    if (err) {
+      req.flash("error_msg", "Could not create post. Please try again.");
+      res.redirect("/posts/add");
+    } else {
+      res.redirect("/posts");
+    }
+  });
 };
 
 exports.getUserPosts = (user, callback) => {
@@ -35,7 +25,7 @@ exports.getUserPosts = (user, callback) => {
 
     const postObjects = [];
 
-    posts.forEach(function(doc) {
+    posts.forEach(function (doc) {
       postObjects.push(doc.toObject());
     });
 
@@ -47,7 +37,6 @@ exports.getPost = (req, res) => {
   const postId = req.params.id;
 
   postModel.getById(postId, (err, post) => {
-
-    res.render('singlepost', { pageTitle: post.title, post: post.toObject()});
+    res.render("singlepost", { pageTitle: post.title, post: post.toObject() });
   });
-}
+};
