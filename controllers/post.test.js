ed284a79a4d70379d539495495521ecff40a3994
jmrnd <jamarlancemiranda@gmail.com>
2024-02-06 17:51:42 +0800

    Added test for success and fail post creation

--- controllers/post.test.js
+++ controllers/post.test.js
@@ -1,2 +1,72 @@
-import { addPost } from "./postController";
-import { jest } from "@jest/globals";
+const sinon = require('sinon');
+const postModel = require('../models/post');
+const postController = require("./postController");
+
+describe('Post controller', () => {
+    let req = {
+        body: {
+            author: 'stswenguser',
+            title: 'My first test post',
+            content: 'Random content'
+        },
+        session: {
+            user: 'stswenguser'
+        },
+        flash: sinon.spy()
+    };
+
+    let error = new Error({ error: 'Some error message' });
+
+    let res = {};
+
+    let expectedResult;
+
+    describe('Create Post', () => {
+        let createPostStub;
+
+        beforeEach(() => {
+            res = {
+                json: sinon.spy(),
+                status: sinon.stub().returns({ end: sinon.spy() }),
+                redirect: sinon.spy()
+            };
+        });
+
+        afterEach(() => {
+            createPostStub.restore();
+        });
+
+        it('should redirect to /posts if post creation is successful', () => {
+
+            expectedResult = {
+                _id: '507asdghajsdhjgasd',
+                title: 'My first test post',
+                content: 'Random content',
+                author: 'stswenguser',
+                date: Date.now()
+            };
+
+            createPostStub = sinon.stub(postModel, 'create').yields(null, expectedResult);
+
+            postController.addPost(req, res);
+
+            sinon.assert.calledWith(postModel.create, req.body);
+            sinon.assert.calledWith(res.json, sinon.match({ title: req.body.title }));
+            sinon.assert.calledWith(res.json, sinon.match({ content: req.body.content }));
+            sinon.assert.calledWith(res.json, sinon.match({ author: req.session.user }));
+            sinon.assert.calledWith(res.redirect, '/posts');
+            sinon.assert.notCalled(req.flash);
+        });
+
+        it('should redirect to /posts/add if post creation fails', () => {
+            createPostStub = sinon.stub(postModel, 'create').yields(error);
+
+            postController.addPost(req, res);
+
+            sinon.assert.calledWith(res.redirect, '/posts/add');
+            sinon.assert.calledWith(req.flash, 'error_msg', 'Could not create post. Please try again.');
+        });
+
+    });
+
+});
--- controllers/postController.js
+++ controllers/postController.js
@@ -1,33 +1,33 @@
 const postModel = require('../models/post');
-// const { validationResult } = require('express-validator');
+const { validationResult } = require('express-validator');
 
 exports.addPost = (req, res) => {
-  // const errors = validationResult(req);
-
-  // if (errors.isEmpty()) {
-    const {
-      title,
-      content
-    } = req.body;
-
-    const author = req.session.user;
-
-    postModel.create({ title, content, author}, (err, post) => {
-      if (err) {
-        req.flash('error_msg', 'Could not create post. Please try again.');
+     const errors = validationResult(req);
+  
+     if (errors.isEmpty()) {
+        const {
+            title,
+            content
+        } = req.body;
+    
+        const author = req.session.user;
+  
+        postModel.create({ title, content, author}, (err, post) => {
+            if (err) {
+                req.flash('error_msg', 'Could not create post. Please try again.');
+                res.redirect('/posts/add');
+            } else {
+                res.json(post);
+                res.redirect('/posts');
+            }
+        });
+    } else {
+        const messages = errors.array().map((item) => item.msg);
+        req.flash('error_msg', messages.join(' '));
         res.redirect('/posts/add');
-      } else {
-        res.redirect('/posts');
-      }
-    });
-  } else {
-    const messages = errors.array().map((item) => item.msg);
-
-    req.flash('error_msg', messages.join(' '));
-    res.redirect('/posts/add');
-  }
-
+    }  
 };
+  
 
 exports.getUserPosts = (user, callback) => {
   postModel.getByUser(user, (err, posts) => {
